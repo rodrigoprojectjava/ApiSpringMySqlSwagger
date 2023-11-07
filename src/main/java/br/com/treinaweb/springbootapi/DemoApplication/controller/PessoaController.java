2156fd67b75c7781d53aaee882efbe2c1bed04b7
Rodrigo Oliveira <projetosrodrigojava@gmail.com>
2023-11-07 12:03:53 -0300

    Feito ajuste nos imports das classes

--- src/main/java/br/com/treinaweb/springbootapi/DemoApplication/controller/PessoaController.java
+++ src/main/java/br/com/treinaweb/springbootapi/DemoApplication/controller/PessoaController.java
@@ -1,15 +1,23 @@
 package br.com.treinaweb.springbootapi.DemoApplication.controller;
 
-import br.com.treinaweb.springbootapi.DemoApplication.entity.Pessoa;
-import br.com.treinaweb.springbootapi.DemoApplication.repository.PessoaRepository;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import br.com.treinaweb.springbootapi.DemoApplication.entity.Pessoa;
+import br.com.treinaweb.springbootapi.DemoApplication.repository.PessoaRepository;
+
+
 
 @RestController
 public class PessoaController {
